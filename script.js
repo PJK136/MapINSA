2429e3b33369e37bcadbe8b3f6f3deabef60fae7
Paul Du <du.paul136@gmail.com>
2018-11-13 10:48:59 +0100

    Added map

---
+++ script.js
@@ -0,0 +1,23 @@
+var map = L.map('map').fitWorld();
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+function onLocationFound(e) {
+        var radius = e.accuracy / 2;
+
+        L.marker(e.latlng).addTo(map)
+                .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+        L.circle(e.latlng, radius).addTo(map);
+}
+
+function onLocationError(e) {
+        alert(e.message);
+}
+
+map.on('locationfound', onLocationFound);
+map.on('locationerror', onLocationError);
+
+map.locate({setView: true, maxZoom: 16});
