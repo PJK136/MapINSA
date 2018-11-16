2f6e3caba3ccd3365d584bfb3f49b03f4fae5f0b
Gillpoitou <39384177+Gillpoitou@users.noreply.github.com>
2018-11-16 11:08:49 +0100

    Add Layers services

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-var map = L.map('map').fitWorld();
+var map = L.map('map',{
+    center: [45.784130265610656,4.876399709901307],
+    zoom : 16
+});
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-function onLocationFound(e) {
+/*function onLocationFound(e) {
     var radius = e.accuracy / 2;
 
     L.marker(e.latlng).addTo(map)
         .bindPopup("You are within " + radius + " meters from this point").openPopup();
 
-    L.circle(e.latlng, radius).addTo(map);
+    //L.circle(e.latlng, radius).addTo(map);
 }
 
 function onLocationError(e) {
@@ -18,7 +21,7 @@ function onLocationError(e) {
 }
 
 map.on('locationfound', onLocationFound);
-map.on('locationerror', onLocationError);
+map.on('locationerror', onLocationError);*/
 
-map.locate({setView: true, maxZoom: 16});
+//map.locate({setView: true, maxZoom: 16});
 
--- search.js
+++ search.js
@@ -1,3 +1,5 @@
+var TypeEnum = Object.freeze({"Administratif":1,"Sport":2,"Sante":3,"Restaurants":4,"VA":5,"Amphi":6,"Departement":7});
+
 class Place{
     constructor(name,latlng, type){
         this.name = name;
@@ -6,15 +8,54 @@ class Place{
     }
 }
 
-var places = [new Place("Service courrier", L.latLng(45.7834146,4.87807870000006), type)];
+var places = [new Place("Service courrier", L.latLng(45.7834146,4.87807870000006),TypeEnum.Administratif)];
 
 function search(request){
 
     places.forEach(element => {
         if(request == element.name){
-            element.latlng.addTo(map);
-            //break;
+            L.marker(element.latlng).addTo(map);
+        }
+    });
+}
+
+function createLayer(typeName){
+
+    var markers = [];
+    places.forEach(element => {
+        if(typeName == element.type){
+            markers.push(L.marker(element.latlng));
         }
     });
 
+   return L.layerGroup(markers);
 }
+
+var administartif = createLayer(TypeEnum.Administratif);
+
+
+function displayLayer(typeName){
+    switch(typeName){
+        case TypeEnum.Administratif:
+            administartif.addTo(map);
+            break;
+        case TypeEnum.Amphi:
+            administartif.addTo(map);
+            break;
+    }
+}
+
+function hideLayer(typeName){
+    switch(typeName){
+        case TypeEnum.Administratif:
+            map.removeLayer(administartif);
+            break;
+        case TypeEnum.Amphi:
+            administartif.removeTo(map);
+            break;
+    }
+}
+displayLayer(TypeEnum.Administratif);
+setTimeout(function(){
+    hideLayer(TypeEnum.Administratif);
+},2000);
