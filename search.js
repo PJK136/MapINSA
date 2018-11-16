20400c49bb3398d0c2d485c5528f10cb2b52f43f
Gillpoitou <39384177+Gillpoitou@users.noreply.github.com>
2018-11-16 11:38:50 +0100

    Update adding  "database"

--- search.js
+++ search.js
@@ -8,7 +8,17 @@ class Place{
     }
 }
 
-var places = [new Place("Service courrier", L.latLng(45.7834146,4.87807870000006),TypeEnum.Administratif)];
+var places = [new Place("Service courrier", L.latLng(45.7834146,4.87807870000006),TypeEnum.Administratif),
+    new Place("Direction",L.latLng(45.78332720254533,4.878197908401489),TypeEnum.Administratif),
+    new Place("Département Informatique",L.latLng(45.78192708850882,4.872792647703136),TypeEnum.Departement),
+    new Place("Département Génie Electrique",L.latLng(45.78294016834221,4.871694841686804), TypeEnum.Departement),
+    new Place("Département FIMI",L.latLng(45.783969959961736,4.88181542071311), TypeEnum.Departement),
+    new Place("Terrain de Volley Ball",L.latLng(45.784947033744814,4.881475567817688),TypeEnum.Sport),
+    new Place("Association AEDI",L.latLng(45.78192708850882,4.872792647703136),TypeEnum.VA),
+    new Place("Bureau des élèves",L.latLng(45.78408288023195,4.87468957901001),TypeEnum.VA),
+    new Place("Infirmerie",L.latLng(45.78429427547835,4.879103081922608),TypeEnum.Sante),
+    new Place("Beurk",L.latLng(45.78109003726761,4.873552322387695),TypeEnum.Restaurants),
+    new Place("Amphithéatre Gaston Berger",L.latLng(45.78245928286134,4.872044920921326),TypeEnum.Amphi)];
 
 function search(request){
 
@@ -31,16 +41,37 @@ function createLayer(typeName){
    return L.layerGroup(markers);
 }
 
-var administartif = createLayer(TypeEnum.Administratif);
+var administratif = createLayer(TypeEnum.Administratif);
+var sport = createLayer(TypeEnum.Sport);
+var sante = createLayer(TypeEnum.Sante);
+var restaurants = createLayer(TypeEnum.Restaurants);
+var va = createLayer(TypeEnum.VA);
+var amphi = createLayer(TypeEnum.Amphi);
+var departement = createLayer(TypeEnum.Departement);
 
 
 function displayLayer(typeName){
     switch(typeName){
         case TypeEnum.Administratif:
-            administartif.addTo(map);
+            administratif.addTo(map);
+            break;
+        case TypeEnum.Sport:
+            sport.addTo(map);
+            break;
+        case TypeEnum.Sante:
+            sante.addTo(map);
+            break;
+        case TypeEnum.Restaurants:
+            restaurants.addTo(map);
+            break;
+        case TypeEnum.VA:
+            va.addTo(map);
             break;
         case TypeEnum.Amphi:
-            administartif.addTo(map);
+            amphi.addTo(map);
+            break;
+        case TypeEnum.Departement:
+            departement.addTo(map);
             break;
     }
 }
@@ -48,14 +79,29 @@ function displayLayer(typeName){
 function hideLayer(typeName){
     switch(typeName){
         case TypeEnum.Administratif:
-            map.removeLayer(administartif);
+            map.removeLayer(administratif);
+            break;
+        case TypeEnum.Sport:
+            map.removeLayer(sport);
+            break;
+        case TypeEnum.Sante:
+            map.removeLayer(sante);
+            break;
+        case TypeEnum.Restaurants:
+            map.removeLayer(restaurants);
+            break;
+        case TypeEnum.VA:
+            map.removeLayer(va);
             break;
         case TypeEnum.Amphi:
-            administartif.removeTo(map);
+            map.removeLayer(amphi);
+            break;
+        case TypeEnum.Departement:
+            map.removeLayer(departement);
             break;
     }
 }
-displayLayer(TypeEnum.Administratif);
+displayLayer(TypeEnum.Sport);
 setTimeout(function(){
-    hideLayer(TypeEnum.Administratif);
+    hideLayer(TypeEnum.Departement);
 },2000);
