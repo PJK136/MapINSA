41d71c205932434f1e76ea2ef53009014ed6d357
Paul Du <du.paul136@gmail.com>
2018-11-22 21:43:34 +0100

    Add button for localisation and change title

--- script.js
+++ script.js
@@ -8,20 +8,13 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
     id:'mapbox.light',attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-function onLocationFound(e) {
-    var radius = e.accuracy / 2;
-
-    //L.marker(e.latlng).addTo(map)
-    //    .bindPopup("You are within " + radius + " meters from this point").openPopup();
-
-    L.circle(e.latlng, radius).addTo(map);
-}
-
-function onLocationError(e) {
-    alert(e.message);
-}
-
-map.on('locationfound', onLocationFound);
-map.on('locationerror', onLocationError);
-
-map.locate({setView: true, maxZoom: 16});
+var lc = L.control.locate({
+    position: "bottomright",
+    clickBehavior: {
+        inView: 'setView',
+        outOfView: 'setView',
+        inViewNotFollowing: 'setView'
+    }
+});
+lc.addTo(map);
+lc.start();
